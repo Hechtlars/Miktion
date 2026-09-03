7345063882f3c1acc140616658ae1bd999326df6
Hechtlars <larsburkel@online.de>
2026-09-03 15:49:15 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='blasentagebuch-v1.4';
+const CACHE='miktion-v2.0';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
